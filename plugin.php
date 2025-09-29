6999e9513824521cbe25ba15fea6a63bc4d6b520
Justin Tadlock <justintadlock@gmail.com>
2025-09-29 09:01:13 -0500

    Add license and version.

--- plugin.php
+++ plugin.php
@@ -4,11 +4,12 @@
  * Plugin Name:       X3P0: Authors
  * Plugin URI:        https://github.com/x3p0-dev/x3p0-authors
  * Description:       Adds a block for listing post authors.
- * Version:           1.0.0-alpha
+ * Version:           1.0.0
  * Requires at least: 6.8
  * Requires PHP:      8.0
  * Author:            Justin Tadlock
  * Author URI:        https://justintadlock.com
+ * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       x3p0-authors
  */
